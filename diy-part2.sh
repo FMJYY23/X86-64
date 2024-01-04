cdff81b95395a3adb12a5ef6437c26cc68d9e16d
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2024-01-04 14:40:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' pa
 
 sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/R23.11.11/2023/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R23.11.11/2024 SVIP/g' package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
